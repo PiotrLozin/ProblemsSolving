09466c245abfe586244d1937844c922243a0b0c5
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2023-12-13 22:51:11 +0100

    Submit resolve for problem "Merge Sorted Array" from leetCode

---
+++ ProblemsSolving/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ProblemsSolving;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+        int[] nums2 = { 2, 5, 6 };
+
+        int m = 3;
+        int n = 3;
+
+        MergeSortedArray.Merge(ref nums1, m, nums2, n);
+
+        foreach (int element in nums1)
+        {
+            Console.WriteLine(element);
+        }
+    }
+}
