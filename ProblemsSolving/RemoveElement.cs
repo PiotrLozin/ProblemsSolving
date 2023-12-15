a8d93727bfcfb43dc6dd2d0d8565c41081f20853
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2023-12-15 21:12:42 +0100

    Resolve for RemoveElement problem from LeetCode

---
+++ ProblemsSolving/RemoveElement.cs
@@ -0,0 +1,30 @@
+﻿namespace ProblemsSolving
+{
+    public class RemoveElement
+    {
+        public static int removeElement(ref int[] nums, int val)
+        {
+            //Remove all 'val' from array
+            nums = nums.Where(x => x != val).ToArray();
+
+            //Return number of elements in array after removing
+            return nums.Length;
+        }
+    }
+
+    //public static void Main(string[] args)
+    //{
+    //    int[] nums1 = { 1, 2, 3, 0, 0, 0 };
+    //    int[] nums2 = { 2, 5, 6 };
+
+    //    int m = 3;
+    //    int n = 3;
+
+    //    MergeSortedArray.Merge(ref nums1, m, nums2, n);
+
+    //    foreach (int element in nums1)
+    //    {
+    //        Console.WriteLine(element);
+    //    }
+    //}
+}
