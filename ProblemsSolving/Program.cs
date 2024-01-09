42d066148d75f26fc650ea19d477804634f7cb84
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2024-01-09 19:17:07 +0100

    Added solution for Candy problem from LeetCode

--- ProblemsSolving/Program.cs
+++ ProblemsSolving/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using ProblemsSolving.TopInterview150.Easy;
+using ProblemsSolving.TopInterview150.Hard;
 using ProblemsSolving.TopInterview150.Medium;
 using Xunit;
 
@@ -7,12 +8,7 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int[] gas = { 1, 2, 3, 4, 5 };
-        int[] cost = { 3, 4, 5, 1, 2 };
-        Assert.Equal(3, GasStation.gasStation(gas, cost));
-
-        int[] gas1 = { 2,3, 4 };
-        int[] cost1 = { 3, 4, 3 };
-        Assert.Equal(-1, GasStation.gasStation(gas1, cost1));
+        int[] ratings = { 1, 2, 87, 87, 87, 2, 1 };
+        Assert.Equal(13, Candy.candy(ratings));
     }
 }
---
+++ ProblemsSolving/TopInterview150/Hard/Candy.cs
@@ -0,0 +1,51 @@
+﻿using ProblemsSolving.TopInterview150.Medium;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ProblemsSolving.TopInterview150.Hard
+{
+    public class Candy
+    {
+        public int candy(int[] ratings)
+        {
+            int n = ratings.Length;
+            int[] candies = new int[n];
+            Array.Fill(candies, 1);
+
+            for (int i = 1; i < n; i++)
+            {
+                if (ratings[i] > ratings[i - 1])
+                {
+                    candies[i] = candies[i - 1] + 1;
+                }
+            }
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (ratings[i] > ratings[i + 1])
+                {
+                    candies[i] = Math.Max(candies[i], candies[i + 1] + 1);
+                }
+            }
+
+            return candies.Sum();
+        }
+    }
+
+    //public static void Main(string[] args)
+    //{
+    //    int[] gas = { 1, 2, 3, 4, 5 };
+    //    int[] cost = { 3, 4, 5, 1, 2 };
+    //    Assert.Equal(3, GasStation.gasStation(gas, cost));
+
+    //    int[] gas1 = { 2, 3, 4 };
+    //    int[] cost1 = { 3, 4, 3 };
+    //    Assert.Equal(-1, GasStation.gasStation(gas1, cost1));
+    //}
+}
+
