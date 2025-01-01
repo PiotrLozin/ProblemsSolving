c2b31a4b3084722a072faf0e8211d575fc47407b
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-01 18:48:09 +0100

    MaximumScoreAfterSplittingAString solved

---
+++ ProblemsSolving/TopInterview150/Easy/MaximumScoreAfterSplittingAString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Easy
+{
+    public class MaximumScoreAfterSplittingAString
+    {
+        public int MaxScore(string s)
+        {
+            int MaxScore = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                string first = s[..i];
+                string second = s[i..];
+
+                int countOfZeros = first.Count(x => x == '0');
+                int countOfOnes = second.Count(x => x == '1');
+
+                var sum = countOfZeros + countOfOnes;
+                if (sum > MaxScore)
+                {
+                    MaxScore = sum;
+                }
+            }
+
+            return MaxScore;
+        }
+    }
+}
