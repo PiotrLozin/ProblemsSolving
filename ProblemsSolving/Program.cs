0189d935c93c902936cbb2fad789b196a61e7d62
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-03 18:40:07 +0100

    Problem WaysToSplitArray solved.

--- ProblemsSolving/Program.cs
+++ ProblemsSolving/Program.cs
@@ -8,9 +8,8 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        string[] words = { "aba", "bcb", "ece", "aa", "e" };
-        int[][] queries = { new int[] { 0, 2 }, new int[] { 1, 4 }, new int[] { 1, 1 } };
+        int[] nums = { 10, 4, -8, 7 };
 
-        Assert.Equal(new int[] { 2, 3, 0}, VowelStringsProblem.VowelStrings(words, queries));
+        Assert.Equal(2, NumberOfWaysToSplitArray.WaysToSplitArray(nums));
     }
 }
---
+++ ProblemsSolving/TopInterview150/Medium/NumberOfWaysToSplitArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Medium
+{
+    public class NumberOfWaysToSplitArray
+    {
+        public static int WaysToSplitArray(int[] nums)
+        {
+            int waysToSplitArray = 0;
+            long rightSum = 0;
+            long leftSum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                rightSum += nums[i];
+            }
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                leftSum += nums[i];
+                if (leftSum >= rightSum - leftSum)
+                {
+                    waysToSplitArray++;
+                }
+            }
+
+            return waysToSplitArray;
+        }
+    }
+}
