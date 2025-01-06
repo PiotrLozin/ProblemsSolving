e03be185b3cc42e73e38a20d521a21de97012e71
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-06 12:20:24 +0100

    Minimum Number of Operations to Move All Balls to Each Box solved

--- ProblemsSolving/TopInterview150/Medium/MinimumNumberOfOperationsTOMoveAllBallsToEachBox.cs
+++ ProblemsSolving/TopInterview150/Medium/MinimumNumberOfOperationsTOMoveAllBallsToEachBox.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace ProblemsSolving.TopInterview150.Medium
 {
-    internal class MinimumNumberOfOperationsTOMoveAllBallsToEachBox
+    public class MinimumNumberOfOperationsTOMoveAllBallsToEachBox
     {
+        public int[] MinOperations(string boxes)
+        {
+            int[] result = new int[boxes.Length];
+            char[] chars = boxes.ToCharArray();
+
+            for (int i = 0; i < chars.Length; i++) 
+            {
+                for (int j = 0; j < chars.Length; j++)
+                {
+                    if (chars[j] == '1')
+                    {
+                        result[i] += Math.Abs(j - i);
+                    }
+                }
+            }
+
+            return result;
+        }
     }
 }
