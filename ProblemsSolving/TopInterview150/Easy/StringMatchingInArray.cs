4944670835e9ea17894e5a4ba47739a6207d8035
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-07 07:55:08 +0100

    String matching in a array - Solved

---
+++ ProblemsSolving/TopInterview150/Easy/StringMatchingInArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Easy
+{
+    public class StringMatchingInArray
+    {
+        public static IList<string> StringMatching(string[] words)
+        {
+            IList<string> result = new List<string>();
+            foreach (string word in words)
+            {
+                foreach (string word2 in words)
+                {
+                    if (word2.Contains(word) && !word.Equals(word2))
+                    {
+                        result.Add(word);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
