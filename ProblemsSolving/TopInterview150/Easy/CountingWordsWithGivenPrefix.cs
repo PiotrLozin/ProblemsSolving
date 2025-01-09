b55a3e0519b6a703264df07716cdb46bbcec1532
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-09 21:50:39 +0100

    Counting words with given prefix solved.

---
+++ ProblemsSolving/TopInterview150/Easy/CountingWordsWithGivenPrefix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Easy
+{
+    public class CountingWordsWithGivenPrefix
+    {
+        public int PrefixCount(string[] words, string pref)
+        {
+            int count = 0;
+
+            foreach (string word in words)
+            {
+                if (word.StartsWith(pref))
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
