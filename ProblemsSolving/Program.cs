22e842720a4a0b9895882ed50f2190d7d7e64c77
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-09 23:22:48 +0100

    40. Combination Sum II solved.

--- ProblemsSolving/Program.cs
+++ ProblemsSolving/Program.cs
@@ -8,8 +8,18 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        string[] s = { "a", "aba", "ababa", "aa" };
+        int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
+        IList<IList<int>> possibleResults = new List<IList<int>>();
+        IList<int> newResult1 = new List<int> { 1,1,6 };
+        IList<int> newResult2 = new List<int> { 1,2,5 };
+        IList<int> newResult3 = new List<int> { 1,7 };
+        IList<int> newResult4 = new List<int> { 2,6 };
+        possibleResults.Add(newResult1);
+        possibleResults.Add(newResult2);
+        possibleResults.Add(newResult3);
+        possibleResults.Add(newResult4);
 
-        Assert.Equal(4, CountPrefixAndSuffixPairs.CountPrefixSuffixPairs(s));
+
+        Assert.Equal(possibleResults, CombinationSumII.CombinationSum2(candidates, 8));
     }
 }
---
+++ ProblemsSolving/TopInterview150/Medium/CombinationSumII.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Medium
+{
+    public class CombinationSumII
+    {
+        public static IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            List<int> smallerCandidates = new List<int>();
+            IList<IList<int>> result = new List<IList<int>>();
+
+            foreach (int candidate in candidates)
+            {
+                if (candidate <= target)
+                {
+                    smallerCandidates.Add(candidate);
+                }
+            }
+
+            smallerCandidates.Sort();
+
+            for (int i = smallerCandidates.Count - 1; i >= 0; i--)
+            {
+                IList<IList<int>> possibleResults = new List<IList<int>>();
+
+                int j = 0;
+                IList<int> newResult = new List<int> { smallerCandidates[i] };
+                possibleResults.Add(newResult);
+
+                while (smallerCandidates[i] + smallerCandidates[j] <= target)
+                {
+                    if (j >= i)
+                    {
+                        break;
+                    }
+
+                    foreach (IList<int> possibleResult in possibleResults.ToList())
+                    {
+                        newResult = new List<int>(possibleResult);
+                        newResult.Add(smallerCandidates[j]);
+
+                        if (newResult.Sum(x => x) <= target && !possibleResults.Any(x => x.SequenceEqual(newResult)))
+                        {
+                            possibleResults.Add(newResult);
+                        }
+                    }
+
+                    j++;
+                }
+
+                foreach (IList<int> possibleResult in possibleResults)
+                {
+                    if (possibleResult.Sum(x => x) == target)
+                    {
+                        if (!result.Any(existing => existing.SequenceEqual(possibleResult)))
+                        {
+                            result.Add(possibleResult);
+                        }
+                    }
+                }
+            }
+
+            return result;
+
+        }
+    }
+}
