6c05637954fc6df168d1eaf8bf6d861350f78de5
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-10 18:37:45 +0100

    Word Subsets solved

--- ProblemsSolving/Program.cs
+++ ProblemsSolving/Program.cs
@@ -8,18 +8,12 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        int[] candidates = { 10, 1, 2, 7, 6, 1, 5 };
-        IList<IList<int>> possibleResults = new List<IList<int>>();
-        IList<int> newResult1 = new List<int> { 1,1,6 };
-        IList<int> newResult2 = new List<int> { 1,2,5 };
-        IList<int> newResult3 = new List<int> { 1,7 };
-        IList<int> newResult4 = new List<int> { 2,6 };
-        possibleResults.Add(newResult1);
-        possibleResults.Add(newResult2);
-        possibleResults.Add(newResult3);
-        possibleResults.Add(newResult4);
+        string[] words = { "leetcode" };
+        string[] words2 = { "e", "oo" };
+
+        string[] result = { "" };
 
 
-        Assert.Equal(possibleResults, CombinationSumII.CombinationSum2(candidates, 8));
+        Assert.Equal(result, WordSubsets.WordSubset(words, words2));
     }
 }
