28b04b781ada81d8b56c2e4e44e0718fc3936a37
PiotrLozin <63423136+PiotrLozin@users.noreply.github.com>
2025-01-14 21:40:28 +0100

    Find the Prefix Common Array of Two Arrays solved

---
+++ ProblemsSolving/TopInterview150/Medium/FindThePrefixCommonArrayOfTwoArrays.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemsSolving.TopInterview150.Medium
+{
+    public class FindThePrefixCommonArrayOfTwoArrays
+    {
+        public int[] FindThePrefixCommonArray(int[] A, int[] B)
+        {
+            List<int> listA = new List<int>();
+            List<int> listB = new List<int>();
+
+            int[] result = new int[A.Length];
+            result[0] = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                listA.Add(A[i]);
+                listB.Add(B[i]);
+
+                if (i != 0)
+                {
+                    result[i] = result[i - 1];
+                }
+
+                foreach (int common in listA)
+                {
+                    if (listB.Contains(common))
+                    {
+                        result[i]++;
+                        listB.Remove(common);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
